dd3b79c6752a838a5e9057bb9119982e0a9ead5c
Jason-time <noconal+jason-time-github@gmail.com>
2020-07-19 13:53:05 +0900

    helloworld

--- this_is_cpp/this_is_cpp.cpp
+++ this_is_cpp/this_is_cpp.cpp
@@ -1,11 +1,11 @@
-﻿// this_is_cpp.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿
 
 #include <iostream>
 
-int main()
+int main( int argc, char* argv[])
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World!" <<std::endl;
+    return 0;
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
