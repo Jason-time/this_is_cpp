b77173006db27d27369692d134e0fea8e380c93d
Jason-time <noconal+jason-time-github@gmail.com>
2020-07-19 14:30:08 +0900

    042 - AutoSample.cpp - auto 예약어 사용

--- this_is_cpp/this_is_cpp.cpp
+++ this_is_cpp/this_is_cpp.cpp
@@ -3,24 +3,12 @@
 #include <iostream>
 
 int main(int argc, char* argv[])
-{
-    int nAge;
-    std::cout << "나이를 입력하세요." << std::endl;
-    std::cin >> nAge;
+{ 
+    int a = 10;
+    int b(a);
+    auto c(b);
 
-    char szJob[32];
-    std::cout << "직업을 입력하세요." << std::endl;
-    std::cin >> szJob;
-
-    std::string strName;
-    std::cout << "이름을 입력하세요." << std::endl;
-    std::cin >> strName  ;
-
-    std::cout << "당신의 이름은 " << strName << "이고, "
-        << "나이는 " << nAge << "살이며, "
-        << "직업은 " << szJob << "입니다." << std::endl;
-     
-
+    std::cout << a + b + c << std::endl;
     return 0;
 }
-// 039 - CinString.cpp - std::cin 사용법
+// 042 - AutoSample.cpp - auto 예약어 사용
