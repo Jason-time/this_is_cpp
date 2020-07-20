ac20ee52ffb045046edc18c08e1ee22b4b8405fb
Jason-time <noconal+jason-time-github@gmail.com>
2020-07-20 15:48:19 +0900

    / 075 - NamespaceSample.cpp - 네임스페이스 선언 및 정의

--- this_is_cpp/this_is_cpp.cpp
+++ this_is_cpp/this_is_cpp.cpp
@@ -1,22 +1,15 @@
 ﻿#include <iostream>
 
-#define ADD(a,b)((a)+(b))
-
-int Add(int a, int b) {
-	return a + b;
-}
-inline int AddNew(int a, int b) {
-	return a + b;
+namespace TEST {
+	int g_nData = 100;
+	void TesstFunc(void) {
+		std::cout << "TEST::TestFunc()" << std::endl;
+	}
 }
 int main(int argc, char* argv[])
 {
-	int a, b;
-	scanf_s("%d%d", &a, &b);
-
-	printf("ADD(): %d\n", ADD(a, b));
-	printf("Add(): %d\n", Add(a, b));
-	printf("AddNew(): %d\n", AddNew(a, b));
-	
+	TEST::TesstFunc();
+	std::cout << TEST::g_nData << std::endl;
 	return 0;
 }
-// 072- InlineSample.cpp - 인라인 함수
+// 075 - NamespaceSample.cpp - 네임스페이스 선언 및 정의
