7dd158a50367c6225d321b47c35e4d403407cbb5
Jason-time <noconal+jason-time-github@gmail.com>
2020-07-20 19:31:39 +0900

    / 089 - IdSearchSeq4.cpp - using namespace 선언을 적용하기 전

--- this_is_cpp/this_is_cpp.cpp
+++ this_is_cpp/this_is_cpp.cpp
@@ -1,19 +1,16 @@
 ﻿#include <iostream>
 using namespace std;
 
-int nData = 200;
+int nData = 100;
 
 namespace TEST {
-	int nData = 100;
-	void TestFunc(void) {
-		cout << nData << endl;
-	}
+	int nData = 200;
 }
 
  int main(int argc, char* argv[])
 {
-	 TEST::TestFunc();
+	 cout << nData << endl;
 	 
 	return 0;
 }
-// 087 - IdSearchSeq3.cpp - 네임스페이스와 전역 변수의 검색 우선권
+// 089 - IdSearchSeq4.cpp - using namespace 선언을 적용하기 전
